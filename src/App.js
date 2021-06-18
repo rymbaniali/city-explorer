da845516f4d3e6b85f18052dc150ab8aea783f4a
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-18 18:54:57 +0300

    added movies and modules

--- src/App.js
+++ src/App.js
@@ -2,46 +2,60 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export class App extends Component {
- 
+
   constructor(props) {
     super(props);
     this.state = {
       cityName: '',
       cityData: {},
+      moviesData: [],
       lat: '',
       lon: '',
       weatherData: [],
       displayData: false,
+      displayMoviesData: false,
     }
   };
-
   updateCityNameState = (event) => {
-    
+
     this.setState({
       cityName: event.target.value
     });
 
   }
-
   getCityData = async (event) => {
     event.preventDefault();
     await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MY_API}&city=${this.state.cityName}&format=json`).then(locationResponse => {
-
+      console.log(locationResponse);
       this.setState({
         cityData: locationResponse.data[0],
         lat: locationResponse.data[0].lat,
         lon: locationResponse.data[0].lon,
       });
+
+      console.log(this.state.cityData);
       axios.get(`${process.env.REACT_APP_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`).then(weatherResponse => {
         this.setState({
-          weatherData: weatherResponse.data,
+          moviesData: weatherResponse.data,
           displayData: true
         })
-
       });
     });
 
   }
+
+
+  getMoviesData =  (event) => {
+    event.preventDefault();
+
+     axios.get(`${process.env.REACT_APP_URL}/movies?key=${process.env.REACT_MOVIES_MY_API}&city=${this.state.cityName}`).then(moviesResponse => {
+      this.setState({
+        moviesData: moviesResponse.data,
+        displayMoviesData: true
+      });
+    })
+  }
+
 
   render() {
     return (
@@ -54,9 +68,9 @@ export class App extends Component {
           <input onChange={this.updateCityNameState} type="text" />
           <br></br>
           <br></br>
-          <input type="submit" value="get City" />
+          <input type="submit" value="Get City" />
         </form>
-    
+
         {this.state.displayData &&
           <div>
 
